dca5f483f86228dbb4f6dae1d96efca974eb9746
the-moo <104044225+the-moo@users.noreply.github.com>
2023-01-26 14:38:20 -0500

    Add files via upload
    Typo corrected.
    Prompts for user inputs

--- mac_changer_userinput.py
+++ mac_changer_userinput.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#Now with user input features!
+#Now with more security!
 
 import subprocess
 
@@ -8,6 +8,6 @@ new_mac = input("new MAC >")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
