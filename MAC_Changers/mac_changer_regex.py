d38f31f396819278e9b882467d1bd81d969e609b
the-moo <104044225+the-moo@users.noreply.github.com>
2023-02-12 09:13:25 +0000

    added new file

--- MAC_Changers/mac_changer_regex.py
+++ MAC_Changers/mac_changer_regex.py
@@ -3,10 +3,11 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change the MAC address of")
+    parser.add_option("-i", "--interface", dest="interface", help="Interface of MAC to change")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
@@ -21,5 +22,24 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac (interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_result = re.search(br"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_result:
+        return mac_address_result.group(0)
+    else:
+        print ("[-] Could not read MAC address.")
+
 options = get_arguments()
+
+current_mac = get_current_mac (options.interface)
+print ("Current MAC = " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
