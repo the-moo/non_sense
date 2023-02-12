616bbc7e00044c5d05dca9fb559174fccf8ea2a1
the-moo <104044225+the-moo@users.noreply.github.com>
2023-02-12 07:31:22 -0500

    Add files via upload
    Fixed "TypeError: cannot use a string pattern on a bytes-like object" by adding decode

--- MAC_Changers/mac_changer_regex.py
+++ MAC_Changers/mac_changer_regex.py
@@ -1,9 +1,10 @@
 # !/usr/bin/env python
-# Now with multiple functions, and if statements!
+# Now using regex!
 
 import subprocess
 import optparse
 import re
+
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -11,10 +12,11 @@ def get_arguments():
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error ("[-] Please specify an interface, use --help for more info.")
+        parser.error("[-] Please specify an interface, use --help for more info.")
     elif not options.new_mac:
-        parser.error ("[-] Please specify a MAC address, use --help for more info.")
+        parser.error("[-] Please specify a MAC address, use --help for more info.")
     return options
+
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -22,19 +24,21 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-def get_current_mac (interface):
+
+def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_result = re.search(br"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_address_result = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result.decode('utf-8'))
 
     if mac_address_result:
         return mac_address_result.group(0)
     else:
-        print ("[-] Could not read MAC address.")
+        print("[-] Could not read MAC address.")
+
 
 options = get_arguments()
 
-current_mac = get_current_mac (options.interface)
-print ("Current MAC = " + str(current_mac))
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 
 change_mac(options.interface, options.new_mac)
 
