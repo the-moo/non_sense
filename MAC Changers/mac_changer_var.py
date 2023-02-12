7fcb7a4a424f0ee54d79bc940a27e23eb8fdfc94
the-moo <104044225+the-moo@users.noreply.github.com>
2023-02-12 01:34:29 +0000

    new file:   MAC Changers/mac_changer.py 	new file:   MAC Changers/mac_changer_function.py 	new file:   MAC Changers/mac_changer_if.py 	new file:   MAC Changers/mac_changer_many_functions.py 	new file:   MAC Changers/mac_changer_parser.py 	new file:   MAC Changers/mac_changer_userinput.py 	new file:   MAC Changers/mac_changer_userinputHS.py 	new file:   MAC Changers/mac_changer_var.py

---
+++ MAC Changers/mac_changer_var.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Now with variables!
+
+import subprocess
+
+interface = input("interace >")
+new_mac = input("new MAC >")
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
