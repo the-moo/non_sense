196be598b5099c723c40acfc3b3741b7cb051d21
the-moo <104044225+the-moo@users.noreply.github.com>
2023-06-28 08:19:25 +0000

    new version of network scanner added

---
+++ network_scanners/network_scanner_v4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# A basic network scanner with a timeout feature and parsed results
+
+
+import scapy.all as scapy
+
+
+def scan (ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+   
+    for element in answered_list:
+        print(element[1].show())
+        print("----------------------------------------------------")
+
+
+scan("172.16.5.1/24") #IP address to be scanned
